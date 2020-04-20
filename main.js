08f73c32d39ccc096fb3d26b518754d6fa65fabb
vegfjogs <vegard.fjogstad@finn.no>
2020-04-21 00:49:36 +0200

    Begin work on colors

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
 function onClickMap(event) {
     const allImages = document.querySelectorAll('img');
     const mapButtons = document.querySelectorAll('#navbar-map')[0].childNodes;
-    mapButtons.forEach(button => {
-        button.className = 'button-map';
+    mapButtons.forEach((button) => {
+        button.className = 'map';
     });
-    allImages.forEach(image => {
+    allImages.forEach((image) => {
         image.style.display = 'none';
     });
     const images = document.querySelectorAll(
         `img[data-map=${event.target.id}]`
     );
-    images.forEach(image => {
+    images.forEach((image) => {
         image.style.display = 'block';
     });
     document.getElementById(event.target.id).className += ' active';
@@ -19,44 +19,42 @@ function onClickMap(event) {
 function onClick(event) {
     const allImages = document.querySelectorAll('img');
     if (event.target.id === 'all-locations') {
-        allImages.forEach(image => {
+        allImages.forEach((image) => {
             image.style.display = 'block';
         });
-        const locationButtons = document.querySelectorAll('.button-location');
-        locationButtons.forEach(button => {
-            button.className = 'button-location';
+        const locationButtons = document.querySelectorAll('.location');
+        locationButtons.forEach((button) => {
+            button.className = 'location';
         });
     } else if (event.target.id === 'all-types') {
-        allImages.forEach(image => {
+        allImages.forEach((image) => {
             image.style.display = 'block';
             const typeButtons = document.querySelectorAll('.button-type');
-            typeButtons.forEach(button => {
+            typeButtons.forEach((button) => {
                 button.className = 'button-type';
             });
         });
     } else {
-        allImages.forEach(image => {
+        allImages.forEach((image) => {
             image.style.display = 'none';
         });
         let query = '';
         const className = event.target.className;
         if (className.includes('location')) {
             query = `img[data-location='${event.target.id}']`;
-            const locationButtons = document.querySelectorAll(
-                '.button-location'
-            );
-            locationButtons.forEach(button => {
-                button.className = 'button-location';
+            const locationButtons = document.querySelectorAll('.location');
+            locationButtons.forEach((button) => {
+                button.className = 'location';
             });
         } else {
             query = `img[data-type='${event.target.id}']`;
             const typeButtons = document.querySelectorAll('.button-type');
-            typeButtons.forEach(button => {
+            typeButtons.forEach((button) => {
                 button.className = 'button-type';
             });
         }
         const images = document.querySelectorAll(query);
-        images.forEach(image => {
+        images.forEach((image) => {
             if (image.dataset.map === currentlyActiveMap()) {
                 image.style.display = 'block';
             }
@@ -67,5 +65,5 @@ function onClick(event) {
 }
 
 function currentlyActiveMap() {
-    return document.getElementsByClassName('button-map active')[0].id;
+    return document.getElementsByClassName('map active')[0].id;
 }
